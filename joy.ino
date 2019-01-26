375292279da2cc457c6a298a089ed70b97c959ec
theycallmelj <43352450+theycallmelj@users.noreply.github.com>
2019-01-26 16:57:02 -0500

    methodized the proccess of controlling the motor

--- joy.ino
+++ joy.ino
@@ -4,15 +4,15 @@ int joyPin2 = 1;                 // slider variable connecetd to analog pin 1
 int maxPosResistor = 255;
 int minPosResistor = 0;
 
-int motorPin1 = 9;
-int motorPin2 = 10;
+int motorP1 = 9;
+int motorP2 = 10;
 
 
 int x;
 void setup() {
   // put your setup code here, to run once:
-  pinMode(motorPin1, OUTPUT); // Declare the output for one polarity of the motor driver
-  pinMode(motorPin2, OUTPUT); // Declare the output for one polarity of the motor driver
+  pinMode(motorP1, OUTPUT); // Declare the output for one polarity of the motor driver
+  pinMode(motorP2, OUTPUT); // Declare the output for one polarity of the motor driver
   x = 0;
 }
 
@@ -21,30 +21,10 @@ void loop() {
  int value2;
   // reads the value of the variable resistor 
   value1 = analogRead(joyPin1); 
+  motorPID(value1,motorP1, motorP2);
   
-  if(value1 < (maxPosResistor -minPosResistor) /2 ){//adds forward momentum
-    digitalWrite(motorPin1, LOW);
-    x+= value1;
-    analogWrite(motorPin2, x);
-  }
-  else if(value1 > (maxPosResistor -minPosResistor) /2){//adds backword momentum
-    digitalWrite(motorPin2, LOW);
-    x+= value1;
-    analogWrite(motorPin1, x);
-  }
-  else{// this code ramps it down
-    if(x > 0){//kills forward momentum
-      digitalWrite(motorPin1, LOW);
-      x-= 1;
-      analogWrite(motorPin2, x);
-    }
-    else{//kills backword momentum
-      digitalWrite(motorPin2, LOW);
-      x-= 1;
-      analogWrite(motorPin1, x);
-    }
     
-  }
+  
 
   
   // this small pause is needed between reading
@@ -54,4 +34,33 @@ void loop() {
   value2 = analogRead(joyPin2); 
 
  // add code for the moving up and down
+}
+
+
+
+  void motorPID(int val, int motorPin1, int motorPin2){
+    if(val < (maxPosResistor -minPosResistor) /2 ){//adds forward momentum
+      digitalWrite(motorPin1, LOW);
+      x+= val;
+      analogWrite(motorPin2, x);
+    }
+    else if(val > (maxPosResistor -minPosResistor) /2){//adds backword momentum
+      digitalWrite(motorPin2, LOW);
+      x+= val;
+      analogWrite(motorPin1, x);
+    }
+    else{// this code ramps it down
+      if(x > 0){//kills forward momentum
+        digitalWrite(motorPin1, LOW);
+        x-= 1;
+        analogWrite(motorPin2, x);
+      }
+      else{//kills backword momentum
+        digitalWrite(motorPin2, LOW);
+        x-= 1;
+        analogWrite(motorPin1, x);
+      }
+    }
+  
+  
 }
